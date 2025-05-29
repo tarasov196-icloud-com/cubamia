97b23b4213a277627ad96409d1547c56dab03592
Ilya <tarasov190@icloud.com>
2025-05-29 21:53:21 +0300

    homework5 - bug fixes

--- src/Main.java
+++ src/Main.java
@@ -6,50 +6,46 @@ public class Main {
         int clientOS = 1;
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке ");
-        } else if (clientOS == 1) {
+        } else {
             System.out.println("Установите версию приложения для Android по ссылке ");
-            System.out.println("Задача 2");
-            clientOS = 0;
-            int clientDeviceYear = 2014;
-            if (clientOS == 0) {
-                if (clientDeviceYear < 2015) {
-                    System.out.println("Установите облегченную версию приложения для iOS по ссылке ");
-                } else {
-                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
-                }
-                if (clientOS == 1) {
-                    if (clientDeviceYear < 2015) {
-                        System.out.println("Установите версию приложения для iOS по ссылке ");
-                    } else {
-                        System.out.println("Установите версию приложения для Android по ссылке");
-                    }
-                }
-            }
         }
-        System.out.println("Задача 3");
-        int yearLeap = 2024;
-        if (yearLeap > 1584) {
-            if ((yearLeap % 400 == 0) || (yearLeap % 4 == 0 && yearLeap % 100 != 0)) {
-                System.out.println(yearLeap + " год является високосным ");
-            } else {
-                System.out.printf(yearLeap + " год не является високосным ");
-            }
+        System.out.println("Задача 2");
+        clientOS = 0;
+        int clientDeviceYear = 2014;
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке ");
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+
+            System.out.println("Установите облегченную версию приложения для Android по ссылке ");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
 
+
+        System.out.println("Задача 3");
+        int yearLeap = 2024;
+
+        if (yearLeap > 1584 && ((yearLeap % 4 == 0 && yearLeap % 100 != 0) || (yearLeap % 400 == 0))) {
+            System.out.println(yearLeap + " год является високосным ");
+        } else {
+            System.out.println(yearLeap + " год не является високосным ");
+        }
         System.out.println("Задача 4");
         int deliveryDistance = 95;
-        int deliveryTime = 0;
-        if (deliveryDistance <= 20) {
-            deliveryTime = 1;
+        int deliveryDay = 0;
+        if (deliveryDistance >=0 && deliveryDistance<= 20 ) {
+            deliveryDay = 1;
         } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            deliveryTime = 2;
+            deliveryDay = 2;
         } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            deliveryTime = 3;
+            deliveryDay = 3;
         } else {
             System.out.println("Доставка невозможна");
-            return;
         }
-        System.out.println("Потребуется дней " + deliveryTime);
+        System.out.println("Потребуется дней " + deliveryDay);
         System.out.println("Задача 5");
         int monthNumber = 12;
         if (monthNumber > 12 || monthNumber < 1) {
@@ -78,4 +74,4 @@ public class Main {
                 System.out.println("Осень");
         }
     }
-    }
+}
